91b5451f8dd66384e23a997bb70b378d25e2d420
jingx <liqingsanjin@163.com>
2019-04-12 09:56:16 +0800

    add userService

--- cmd/main.go
+++ cmd/main.go
@@ -2,13 +2,21 @@ package main
 
 import (
 	"fmt"
+	"net"
+	"os"
+	"runtime"
+	"userService/pb"
+	"userService/pkg/userservice"
 
 	"userService/pkg/common"
 	"userService/pkg/model"
 	"userService/pkg/redis"
 
+	"github.com/go-kit/kit/sd/consul"
+	consuld "github.com/hashicorp/consul/api"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
+	"google.golang.org/grpc"
 )
 
 var (
@@ -20,6 +28,12 @@ var (
 
 	redisHost = "localhost"
 	redisPort = 6379
+
+	grpcHost = "localhost"
+	grpcPort = 5001
+
+	consulHost = "localhost"
+	consulPort = 8500
 )
 
 func main() {
@@ -27,7 +41,6 @@ func main() {
 	if err = parseConfigFile(); err != nil {
 		logrus.Fatal("解析配置文件错误", err)
 	}
-	logrus.Infoln(mysqlHost, mysqlPort, mysqlUser, mysqlPassword, mysqlDB, redisHost, redisPort)
 	common.RedisClientService = redis.New(fmt.Sprintf("%s:%d", redisHost, redisPort))
 	common.DB, err = model.NewDB(&model.Options{
 		User:     mysqlUser,
@@ -39,16 +52,41 @@ func main() {
 		logrus.Fatal("启动mysql错误", err)
 	}
 
+	consulClient, err := newConsulClient(fmt.Sprintf("%s:%d", consulHost, consulPort))
+	if err != nil {
+		logrus.Fatal("连接consul失败", err)
+	}
+
+	go func() {
+		if err := runGRPCServer(fmt.Sprintf("%s:%d", grpcHost, grpcPort)); err != nil {
+			logrus.Fatal("grpc server shutdown", err)
+		}
+	}()
+
+	err = registerConsulService(consulClient, "userService", grpcHost, grpcPort)
+	if err != nil {
+		logrus.Errorln("注册userService失败", err)
+	}
+	runtime.Goexit()
+	logrus.Infoln("启动成功")
 }
 
 func parseConfigFile() error {
+	fileName := os.Getenv("CONFIG_FILE")
+
 	viper.SetConfigType("toml")
 	viper.AddConfigPath("./configs/")
+
+	if fileName != "" {
+		viper.SetConfigFile(fileName)
+	}
 
 	err := viper.ReadInConfig()
 	if err != nil {
 		return err
 	}
+	settings := viper.AllSettings()
+	logrus.Infoln(settings)
 
 	mysqlHost = viper.GetString("db.mysql.host")
 	mysqlPort = viper.GetInt("db.mysql.port")
@@ -58,5 +96,43 @@ func parseConfigFile() error {
 
 	redisHost = viper.GetString("db.redis.host")
 	redisPort = viper.GetInt("db.redis.port")
+
+	grpcHost = viper.GetString("grpc.host")
+	grpcPort = viper.GetInt("grpc.port")
+
+	if host := os.Getenv("GRPC_HOST"); host != "" {
+		grpcHost = host
+	}
+
+	consulHost = viper.GetString("consul.host")
+	consulPort = viper.GetInt("consul.port")
 	return nil
+}
+
+func newConsulClient(addr string) (consul.Client, error) {
+	consulClient, err := consuld.NewClient(&consuld.Config{
+		Address: addr,
+	})
+	if err != nil {
+		return nil, err
+	}
+	return consul.NewClient(consulClient), nil
+}
+
+func registerConsulService(client consul.Client, name string, host string, port int) error {
+	return client.Register(&consuld.AgentServiceRegistration{
+		Address: host,
+		Port:    port,
+		Name:    name,
+	})
+}
+
+func runGRPCServer(addr string) error {
+	l, err := net.Listen("tcp", addr)
+	if err != nil {
+		return err
+	}
+	svr := grpc.NewServer()
+	pb.RegisterUserServer(svr, userservice.New())
+	return svr.Serve(l)
 }
--- pkg/userservice/user.go
+++ pkg/userservice/user.go
@@ -1,1 +1,16 @@
 package userservice
+
+import (
+	"context"
+	"userService/pb"
+)
+
+type userService struct{}
+
+func (u *userService) Login(ctx context.Context, in *pb.LoginRequest) (*pb.LoginReply, error) {
+	return nil, nil
+}
+
+func New() pb.UserServer {
+	return &userService{}
+}
