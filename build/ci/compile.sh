5f28b5d0af003a51e2635fd4c27225d1d0c28a5c
jingx <liqingsanjin@163.com>
2019-04-18 14:08:35 +0800

    fix compile

--- build/ci/compile.sh
+++ build/ci/compile.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 mkdri pkg/pb
 protoc -I ./api --go_out=plugins=grpc:./pkg/pb/ api/user.proto
-GOOS=linux go build -o userservice cmd/userservice/main.go
+GOOS=linux go build -ldflags="-s -w" -o userservice cmd/userservice/main.go
