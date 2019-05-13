253b7e3e9f1a64615c265a1e120c52b0dbf24236
jingx <liqingsanjin@163.com>
2019-05-13 15:22:40 +0800

    fix

--- build/ci/compile_userservice.sh
+++ build/ci/compile_userservice.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 mkdir pkg/pb
 protoc -I ./api --go_out=plugins=grpc:./pkg/pb/ api/*.proto
-CGO_ENABLED=0 GOARCH=amd64 GOOS=linux go build -tags netgo -ldflags="-s -w" -tags netgo -v -o userservice cmd/userservice/main.go
+CGO_ENABLED=0 GOARCH=amd64 GOOS=linux go build -tags netgo -ldflags="-s -w" -tags netgo -o userservice cmd/userservice/main.go
