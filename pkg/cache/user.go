738e6bfe6d2f489ce638b3fc9d7c5a77ca625f8b
jingx <liqingsanjin@163.com>
2019-05-30 14:43:54 +0800

    fix

--- pkg/cache/user.go
+++ pkg/cache/user.go
@@ -11,6 +11,5 @@ func GetUserInfo(client *redis.Client, userId string) (string, error) {
 }
 
 func SetUserInfo(client *redis.Client, userId string, value string, expiredAt time.Duration) error {
-
 	return client.Set(userId, value, expiredAt).Err()
 }
